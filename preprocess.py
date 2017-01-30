d5f66555c293a4b15512df2446265483d5825954
rppgithub <rppgithub@users.noreply.github.com>
2017-01-29 17:40:14 -0800

    Add files via upload

--- preprocess.py
+++ preprocess.py
@@ -1,7 +1,12 @@
 import cv2
+
+def crop_image(img):
+    crop_img = img[60:140,0:320]
+    return crop_img
+
 def image_preprocessing(img,img_cols,img_rows):
-    """preproccesing training data to keep only S channel in HSV color space, and resize to 16X32"""
+    """keep only S channel in HSV color space, and resize to 16X32"""
+    #cropped_img = crop_image(img)
     resized = cv2.resize((cv2.cvtColor(img, cv2.COLOR_RGB2HSV))[:,:,1],(img_cols,img_rows))
+
     return resized
-
-
